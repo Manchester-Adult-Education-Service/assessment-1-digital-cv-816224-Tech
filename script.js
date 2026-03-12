8efc5e90b59499030d912671cd8ac4a9b3dc527d
816224-Tech <816224@manadulted.ac.uk>
2026-03-12 15:06:41 +0000

    Tasks Complete

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const mainContainer = document.querySelector('main');
+const headerBar = document.querySelector('header');
+const subTitles = document.querySelector('h2');
+const allSkills = document.querySelectorAll('li');
+
+allSkills.forEach(skill => {
+    if (skill.textContent.includes("Python")) {
+       skill.style.color = "#1b100a";
+       skill.style.fontWeight = "bold";
+    }
+});
+
 const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
 
+
 if (hour < 12) {
-    nameHeading.textContent = "Good Morning, [Kuda]";
+    nameHeading.textContent = "Good Morning [Kuda]";
 } else if (hour < 18) {
-    nameHeading.textContent = "Good Afternoon, [Kuda]";
+    nameHeading.textContent = "Good Afternoon [Kuda]";
 } else {
-    nameHeading.textContent = "Good Evening, [Kuda]";
+    nameHeading.textContent = "Good Evening [Kuda]";
 }
 
 console.log("The current hour is: " + hour);
