da6585ea3325b72968eed3f9ee51a4d3fe168d2b
816224-Tech <816224@manadulted.ac.uk>
2026-03-26 15:07:32 +0000

    Integrated Quote API with loading and error states

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const quoteBtn = document.getElementById('quote-btn');
+const quoteBtn = document.getElementById('quote-text');
+const quoteBtn = document.getElementById('quote-author');
+
 const mainContainer = document.querySelector('main');
 const headerBar = document.querySelector('header');
 const subTitles = document.querySelector('h2');
@@ -15,11 +19,11 @@ const hour = new Date().getHours();
 
 
 if (hour < 12) {
-    nameHeading.textContent = "Good Morning [Kuda]";
+    nameHeading.textContent = "Good Morning";
 } else if (hour < 18) {
-    nameHeading.textContent = "Good Afternoon [Kuda]";
+    nameHeading.textContent = "Good Afternoon";
 } else {
-    nameHeading.textContent = "Good Evening [Kuda]";
+    nameHeading.textContent = "Good Evening";
 }
 
 console.log("The current hour is: " + hour);
@@ -28,3 +32,13 @@ document.getElementById('copyright').textContent = newDate().getFullYear();
 
 const personalStatement = document.querySelector('.highlight')
 personalStatement.style.backgroundColor = '#2e2a2a';
+
+quoteBtn.addEventListener('click', function() {
+    quoteText.textContent = "Loading"
+    fetch('https://dummyjson.com/quotes/random')
+    .then(response => response.json())
+    .then(data => {
+    .catch(error => {quoteText.textContent = Could not connect to the quote server});
+        quoteText.textContent = '- ${data.author}';
+    ));
+));
